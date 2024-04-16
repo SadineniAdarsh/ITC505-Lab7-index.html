230a8c992fff019eb5bfaa82bb8784ff03dde28d
Sadineni Adarsh <104515754+SadineniAdarsh@users.noreply.github.com>
2024-04-16 11:52:27 -0700

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.get('/do_a_random', (req, res) => {
   res.send(`Your number is: ${Math.floor(Math.random() * 100) + 1}`)
 })
 // Setup static page serving for all the pages in "public"
-const publicServedFilesPath = path.join(__dirname, 'public')
+const publicServedFilesPath = path.join(__dirname, 'index.html')
 server.use(express.static(publicServedFilesPath))
 // The server uses port 80 by default unless you start it with the extra
 // command line argument 'local' like this:
